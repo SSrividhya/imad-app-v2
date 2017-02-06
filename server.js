1717cc0af99dbb785d64963fce280ce8bda6c323
Srividhya <svidhya2512@gmail.com>
2017-02-06 19:20:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one',function(req,res)
+app.get('/article-one',function(req,res)
 {
     res.send("article one reqstd");
 });
-app.get('/article two',function(req,res)
+app.get('/article-two',function(req,res)
 {
     res.send("article two reqstd");
 });
